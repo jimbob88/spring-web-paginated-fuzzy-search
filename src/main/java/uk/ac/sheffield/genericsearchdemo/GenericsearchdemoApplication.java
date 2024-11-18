98d9230248bec9761c0c8dbc207e6874dc8a352c
James Blackburn <blackburnfjames@gmail.com>
2024-11-18 13:51:06 +0000

    chore: initiate template repository

---
+++ src/main/java/uk/ac/sheffield/genericsearchdemo/GenericsearchdemoApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.sheffield.genericsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericsearchdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericsearchdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/ac/sheffield/genericsearchdemo/GenericsearchdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.sheffield.genericsearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericsearchdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
